55548c9646ddddd6e3e1aa5e4463f01735e632a3
lee_wolf_linux <contact@email.com>
2022-08-14 22:39:39 +0900

    complete chrome extension

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -2,29 +2,46 @@ let myLinks = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const linksEl = document.getElementById("links-el");
+const tabBtn = document.getElementById("tab-btn");
+const deleteBtn = document.getElementById("delete-btn");
 
-let savedLinks = JSON.parse(localStorage.getItem("myLinks"));
+const  savedLinks = JSON.parse(localStorage.getItem("myLinks"));
 if (savedLinks) {
     myLinks = savedLinks;
-    renderLinks();
+    render(myLinks);
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.removeItem("myLinks");
+    myLinks = [];
+    render(myLinks);
+});
+
+tabBtn.addEventListener("click", function() {
+    // Read more about the Chrome extensions API
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLinks.push(tabs[0].url);
+        localStorage.setItem("myLinks", JSON.stringify(myLinks));
+        render(myLinks);
+    });
+});
 
 inputBtn.addEventListener("click", function() {
     if (inputEl.value !== "") {
         myLinks.push(inputEl.value);
         inputEl.value = "";
         localStorage.setItem("myLinks", JSON.stringify(myLinks));
-        renderLinks();
+        render(myLinks);
     }
 });
 
-function renderLinks() {
+function render(links) {
     let listItems = "";
-    for (let i = 0; i < myLinks.length; i++) {
+    for (let i = 0; i < links.length; i++) {
         listItems += `
             <li>
-                <a target="_blank" href="${myLinks[i]}">
-                    ${myLinks[i]}
+                <a target="_blank" href="${links[i]}">
+                    ${links[i]}
                 </a>
             </li>
         `
